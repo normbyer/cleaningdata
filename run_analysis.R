3d856c73bb7174da2d6259e5342d5ce449e00809
Norman Byer <normbyer@gmail.com>
2016-01-29 11:36:22 -0500

    initial commit

---
+++ run_analysis.R
@@ -0,0 +1,35 @@
+run_analysis <-function(){
+  ##read column and row names
+  features = read.table("features.txt",colClasses = "character")[2][[1]]
+  activities = read.table("activity_labels.txt", colClasses = "character")[[2]]
+  
+  ##read and filter test data 
+  testSubjects = read.table("test/subject_test.txt",colClasses = "numeric")[[1]]
+  testTable = read.table("test/X_test.txt",colClasses = "numeric")
+  testLabels = read.table("test/y_test.txt",colClasses = "numeric")[[1]]
+  colnames(testTable) = features
+  testTable = testTable[grep("std|mean",features,ignore.case = TRUE)]
+  testTable["activity"] = activities[testLabels]
+  testTable["subject"] = testSubjects
+  
+  ##read and filter train data
+  trainSubjects = read.table("train/subject_train.txt",colClasses = "numeric")[[1]]
+  trainTable = read.table("train/X_train.txt",colClasses = "numeric")
+  trainLabels = read.table("train/y_train.txt",colClasses = "numeric")[[1]]
+  colnames(trainTable) = features
+  trainTable = trainTable[grep("std|mean",features,ignore.case = TRUE)]
+  trainTable["activity"] = activities[trainLabels]
+  trainTable["subject"] = trainSubjects
+  
+  ##combined data set
+  combinedTable = rbind(testTable,trainTable)
+  
+  notActivityNorSubject = !(names(combinedTable) %in% c("activity", "subject")) 
+  ##Calculate averages of each varianle for each activity
+  byActivityTable = apply(combinedTable[,notActivityNorSubject],2,function(a){tapply(a,combinedTable$activity,mean)})
+  ##Calculate averages of each varianle for each subject
+  bySubjectTable = apply(combinedTable[,notActivityNorSubject],2,function(a){tapply(a,combinedTable$subject,mean)})
+  
+  ##return data set and calculated averages
+  list(combinedTable,byActivityTable,bySubjectTable)
+}
